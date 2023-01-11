76e5bf324944f1240acfe75c114fb3bdae02c2c1
Zach Boemer <49701027+zachboemer@users.noreply.github.com>
2023-01-11 16:40:09 -0600

    moving to another repo

--- client/news-pint/src/api/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://your-app-name.herokuapp.com',
-});
-
-export default api;
