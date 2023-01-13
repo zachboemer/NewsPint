945ef81adc4d07db25175d1a95a59015bfa01146
Zach Boemer <49701027+zachboemer@users.noreply.github.com>
2023-01-13 16:26:22 -0600

    Update app.py
    - organized
    - added an endpoint
    - some comments for clarity

--- app.py
+++ app.py
@@ -4,27 +4,35 @@ from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 from models import Article, db
 
-# from models import db, Article
-# from newsapi import NewsApiClient
-
 load_dotenv()
-
 app = Flask(__name__)
-# db_url = os.environ['DATABASE_URL']
-#
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'].replace(
     'postgres://', 'postgresql://', 1)
-
 db.init_app(app)
 
+# do some cors
+# do some jwt
 
-# get all articles
+# -------- ROUTES ---------
+
+# get all articles - this is kinda just for testing, probably gonna remove
+
+
 @app.route('/articles', methods=['GET'])
 def get_articles():
     articles = Article.query.all()
     return jsonify([article.to_dict() for article in articles])
 
-# more routes here
+# returns the articles associated with the date -- if no date passed in, returns articles with the most recent retrieved_at date
+
+
+@app.route('/pint-of-day/<date>', methods=['GET'])
+def get_pint_of_day(date):
+    # TODO
+    return {}
+
+# maybe routes for getting articles of a certain category
+# also gotta add that column for category before we do that
 
 
 if __name__ == '__main__':
