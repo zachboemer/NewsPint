2cf0b27cbe49428fc7a320684ecb71d5cc521b23
Zach Boemer <49701027+zachboemer@users.noreply.github.com>
2023-01-23 15:16:19 -0600

    formatting and adding comments

--- update_db_NEW.py
+++ update_db_NEW.py
@@ -17,14 +17,15 @@ def main():
     # setting up connection to our DB
     db_url = os.environ['DATABASE_URL']
     conn = psycopg2.connect(db_url, sslmode='require')
+
+    # declare empty arrays
+    articlesToAdd = []
+    domains = []
+
     # no science category because the articles are low quality
     categories = ['technology', 'business',
                   'entertainment', 'sports', 'health']
-
-    domains = []
-    for category in categories:
-        domains.append(sourceListToString(newsapi.get_sources(category=category,
-                                                              language='en')))
+    queries = ['esports', 'politics', 'city planning']
 
     # to param
     today = datetime.today()
@@ -35,8 +36,12 @@ def main():
     # from param
     yesterdayDatetime = todayDatetime - timedelta(days=1)
     formattedYesterdayDatetime = yesterdayDatetime.isoformat()
-
+    # gets comma seperated list of sources of each category
+    for category in categories:
+        domains.append(sourceListToString(newsapi.get_sources(category=category,
+                                                              language='en')))
     for categoryDomains in domains:
+        numOfArticlesAddedOfCategory = 0
         response = newsapi.get_everything(
             domains=categoryDomains,
             exclude_domains='',
@@ -46,11 +51,11 @@ def main():
             sort_by='popularity',
             page_size=10
         )
-        numOfArticlesAddedOfCategory = 0
-        articlesToAdd = []
+        # just adds the first two articles in the response
         for article in response['articles']:
             if numOfArticlesAddedOfCategory == 2:
                 break
+            # code for only allowing 1 of each source - we werent't getting a lot of articles in response so commented this out for now
             # sourceAlreadyAdded = False
             # for filteredArticle in articlesToAdd:
             #     if (article['source']['name'] == filteredArticle['source']['name']):
@@ -60,31 +65,46 @@ def main():
             #     continue
             articlesToAdd.append(article)
             numOfArticlesAddedOfCategory += 1
-        # this code once we have added the articles from each category into articlesToAdd[]
-        for article in articlesToAdd:
-            source_name = article['source']['name']
-            title = article['title']
-            author = article['author']
-            publish_date = article['publishedAt']
-            retrieval_date = formattedTodayDatetime
-            url = article['url']
-            image_url = article['urlToImage']
-            summary = article['description']
 
-            with conn.cursor() as cur:
-                cur.execute(
-                    "INSERT INTO articles (source_name, title, author, publish_date, retrieval_date, url, image_url, summary) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+    for query in queries:
+        response = newsapi.get_everything(
+            q=query,
+            exclude_domains='',
+            from_param=formattedYesterdayDatetime,
+            to=formattedTodayDatetime,
+            language='en',
+            sort_by='popularity',
+            page_size=10)
+        numOfArticlesAdded = 0
+        for article in response['articles']:
+            if numOfArticlesAdded == 2:
+                break
+            articlesToAdd.append(article)
+            numOfArticlesAdded += 1
+    # traverse through articlesToAdd, and then insert one by one into articles table
+    for article in articlesToAdd:
+        source_name = article['source']['name']
+        title = article['title']
+        author = article['author']
+        publish_date = article['publishedAt']
+        retrieval_date = formattedTodayDatetime
+        url = article['url']
+        image_url = article['urlToImage']
+        summary = article['description']
 
-                    (source_name, title, author, publish_date,
-                        retrieval_date, url, image_url, summary)
-                )
+        with conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO articles (source_name, title, author, publish_date, retrieval_date, url, image_url, summary) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+
+                (source_name, title, author, publish_date,
+                    retrieval_date, url, image_url, summary)
+            )
     conn.commit()
     conn.close()
-    # print("domains: " + categoryDomains)
-    # print("response: ")
-    # print(response)
 
     return
+
+# returns a comma seperated list of domains to be included for each category's search
 
 
 def sourceListToString(sources_data):
